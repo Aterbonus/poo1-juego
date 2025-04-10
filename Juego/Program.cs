1c7eb195992c164f22b83ce473d4e8cc7fc2f729
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2025-04-10 10:39:56 -0400

    pedir un numero para la vida

--- Juego/Program.cs
+++ Juego/Program.cs
@@ -3,5 +3,27 @@
 class Juego {
     public static void Main() {
         Console.WriteLine("Hola Mundo. Vamos a batallar!");
+
+        Console.WriteLine("Dame la vida del personaje: ");
+        int vida = 0;
+
+        do {
+            try {
+                vida = int.Parse(Console.ReadLine());
+
+                if (vida < 10 || vida > 100) {
+                    Console.WriteLine("La vida debe estar entre 10 y 100");
+                    continue;
+                }
+
+                break;
+            } catch {
+                Console.WriteLine("Escriba un número");
+            }
+
+        } while(true);
+        
+
+        Console.WriteLine($"La vida es: {vida}");
     }
 }
